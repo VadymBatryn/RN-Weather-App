c8cc8f5f86e9f79ebc4bdb4e0acc311ecad7045b
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-28 15:04:37 +0300

    feat: add search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {fetchWeeklyWeather} from '../../store/thunk/asyncFetchWeather';
+import styles from './Search.style';
+
+const Search: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+
+  const [city, setCity] = useState('');
+
+  const searchCityHandler = () => {
+    setLoading(true);
+    dispatch(fetchWeeklyWeather(city.trim()));
+    setCity('');
+    setLoading(false);
+  };
+
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        placeholder="Enter a City"
+        placeholderTextColor="#999"
+        style={styles.searchInput}
+        onChangeText={text => setCity(text)}
+        value={city}
+      />
+      <TouchableOpacity style={styles.container} onPress={searchCityHandler}>
+        {loading ? (
+          <ActivityIndicator size="small" color="#fff" />
+        ) : (
+          <Image
+            source={require('../../assets/loupe.png')}
+            style={styles.searchIcon}
+          />
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Search;
