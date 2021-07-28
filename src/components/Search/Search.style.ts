21ffd8d83150614a638658f08ae68e8a1b269698
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-28 15:04:55 +0300

    feat: add styling for search component

---
+++ src/components/Search/Search.style.ts
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  searchContainer: {
+    height: 40,
+    borderRadius: 20,
+    marginHorizontal: 20,
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  searchInput: {
+    height: 40,
+    width: '90%',
+    color: '#fff',
+  },
+  searchIcon: {
+    height: '100%',
+    width: '100%',
+    tintColor: '#999',
+  },
+  container: {
+    width: 20,
+    height: 20,
+  },
+});
