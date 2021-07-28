7935cb3ededa2701464ba1dc1d79d790659a9214
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-28 17:30:23 +0300

    feat: add weatherApp slice

---
+++ src/store/slice/weatherAppSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {weeklyWeather} from '../../models/weeklyWeatherModel';
+import {fetchWeeklyWeather} from '../thunk/asyncFetchWeather';
+
+const weatherAppSlice = createSlice({
+  name: 'weather',
+
+  initialState: {
+    cityName: '',
+    todayData: new Date().toLocaleDateString(),
+    time: new Date().toLocaleTimeString(),
+    weeklyWeather: null as weeklyWeather,
+  },
+
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchWeeklyWeather.fulfilled, (state, action) => {
+      if (action.payload?.data && action.payload.cityName) {
+        state.weeklyWeather = action.payload.data;
+        state.cityName = action.payload.cityName;
+      }
+    });
+  },
+});
+
+export default weatherAppSlice.reducer;
+export const {} = weatherAppSlice.actions;
