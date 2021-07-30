715f3a6707cb5981aa77c78337e869158d875be3
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-30 10:05:35 +0300

    feat: add weeklyWeather data model for slice

---
+++ src/models/weeklyWeather.ts
@@ -0,0 +1,58 @@
+type List = {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    },
+  ];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain: {
+    number: number;
+  };
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+};
+
+export type weeklyWeather = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: List[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+} | null;
