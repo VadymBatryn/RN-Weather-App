d6008a927cfd46bee1223c76565d77663b4e280d
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-30 10:36:21 +0300

    feat: fetch data and set it to component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,21 +9,41 @@ import Search from '../components/Search/Search';
 import WeatherContainer from '../components/WeatherContainer/WeatherContainer';
 import WeeklyWeather from '../components/WeeklyWeather/WeeklyWeather';
 import TodayWeatherDetails from '../components/TodayWeatherDetails/TodayTemperatures';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../store/configureStore';
+import {useEffect} from 'react';
+import {fetchFromStorage} from '../store/thunk/asyncStorageThunk';
 
 const HomeScreen: React.FC = () => {
+  const dispatch = useDispatch();
+
   const weeklyWeather = useSelector(
     (state: RootState) => state.weather.weeklyWeather,
   );
 
-  if (!weeklyWeather) {
+  const cityName = useSelector((state: RootState) => state.weather.cityName);
+
+  useEffect(() => {
+    dispatch(fetchFromStorage());
+  }, [dispatch]);
+
+  const todayWeather = weeklyWeather?.list.filter(dayInfo => {
+    if (new Date(dayInfo.dt * 1000).getDay() === new Date().getDay()) {
+      return dayInfo;
+    }
+  });
+
+  if (!todayWeather) {
     return (
       <View>
         <ImageBackground
           source={require('../assets/698610.jpeg')}
           style={styles.backgroundImage}>
-          <ActivityIndicator size="large" color="#fff" />
+          {cityName ? (
+            <ActivityIndicator size="large" color="#fff" />
+          ) : (
+            <Search />
+          )}
         </ImageBackground>
       </View>
     );
@@ -37,7 +57,14 @@ const HomeScreen: React.FC = () => {
         <SafeAreaView style={styles.screen}>
           <View>
             <Search />
-            <WeatherContainer temperature="" location="" id="" description="" />
+            <WeatherContainer
+              temperature={todayWeather[0].main.temp.toFixed()}
+              location={
+                weeklyWeather?.city.name + ', ' + weeklyWeather?.city.country
+              }
+              id={todayWeather[0].weather[0].icon}
+              description={todayWeather[0].weather[0].description}
+            />
             <TodayWeatherDetails />
             <WeeklyWeather />
           </View>
