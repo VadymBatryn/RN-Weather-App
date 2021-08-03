44d0d3e90e7314a92afff6e24a3c06c45e36d34f
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-03 11:47:06 +0300

    feat: add chartConfig

---
+++ src/constants/chartConfig.ts
@@ -0,0 +1,11 @@
+export default {
+  backgroundGradientFrom: '#fff',
+  backgroundGradientFromOpacity: 0.15,
+  backgroundGradientTo: '#fff',
+  backgroundGradientToOpacity: 0.15,
+  decimalPlaces: 0,
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 5,
+  },
+};
